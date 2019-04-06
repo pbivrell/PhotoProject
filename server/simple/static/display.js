d53a41c40d5ebed62c0f63febf6346eb9d5a5973
Paul Bivrell <prbivrell@gmail.com>
2019-04-06 14:17:45 +1300

    Added nav and title to client code

--- server/simple/static/display.js
+++ server/simple/static/display.js
@@ -25,14 +25,53 @@ $(document).ready(function () {
             }
         }
     };
+
+    var updatePageData = function(){
+        //$("#Nav").InnerHTML("<span>
+        $("#nav").html(makeURLNav());
+        $("#title1").text(pageData.title1);
+        $("#title2").text(pageData.title2);
+        $("#description").text(pageData.description);
+    };
+
+    var makeURLNav = function(){
+        res = "<span>";
+        url = "http://localhost:8080/";
+        path = trim(window.location.pathname, "/");
+        console.log(path);
+        console.log(path.split("/"));
+        if(path == ""){
+            return "<span></span>"
+        }
+        res += '<a href="' + url + '"> ' + "All Photos" + ' </a>';
+        spath = path.split("/");
+        spath.forEach(function(entry, index){
+            console.log(entry, index, spath.length);
+            if(index < spath.length - 1){
+                url += entry + "/";
+                res += '/<a href="' + url + '"> ' + decodeURI(entry) + ' </a>';
+            }
+        });
+        return res + "</span>";
+    }
+
+    var trim = function(data, c){
+        temp = data;
+        if(temp[0] == c){
+            temp = temp.slice(1);
+        }
+        if(temp[temp.length-1] == c){
+            temp = temp.slice(0, temp.length -1);
+        }
+        return temp;
+    }
     
     $(window).on('resize scroll', updateImages);
 
-    $.getJSON("http://localhost:8080/get?path="+window.location.pathname.slice(1), function(data){
+    //$.getJSON("http://localhost:8080/get?path="+window.location.pathname.slice(1), function(data){
+    $.getJSON("http://localhost:8080/json", function(data){
         pageData = data;
-        $("#Title1").text(pageData.title1);
-        $("#Title2").text(pageData.title2);
-        $("#Description").text(pageData.description);
+        updatePageData();
         updateImages();
         updateImages();
         updateImages();
--- server/simple/static/templates/display.tpl
+++ server/simple/static/templates/display.tpl
@@ -1,13 +1,18 @@
 <!DOCTYPE HTML>
+<html>
 <head>
     <script src="http://localhost:8080/static/jquery-3.3.1.min.js"></script>
     <link rel="stylesheet" type="text/css" href="http://localhost:8080/static/display.css">
     <script src="http://localhost:8080/static/display.js"></script>
+    <title>{{.Title}}</title>
 </head>
 
 <body>
-    <p> <span id="Title1"></span> | <span id="Title2"></span></p>
-    <p> <span id="Description"></span> </p>
+    <div id="nav"></div>
+    <div id="top">
+        <p> <span id="title1"></span> | <span id="title2"></span></p>
+        <p id="description"> <span id="description"></span> </p>
+    <div>
     <div class="row">
         <div class="column" id="0">
         </div>
@@ -19,3 +24,7 @@
         </div>
     </div>
 </body>
+</html>
+
+
+
